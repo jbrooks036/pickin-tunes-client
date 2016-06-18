a25421f781f21ace2033a4bca2e52d312c8dfe5e
jbrooks036 <joannbrooks036@gmail.com>
2016-06-18 14:32:59 -0500

    grabbing tune title input value

--- addForm.js
+++ addForm.js
@@ -6,6 +6,7 @@
 	var addLink = document.getElementById("link-add-tune");
   var addView = document.getElementById("add-tune-view");
   var listView = document.getElementById("list-tunes-view");
+  var titleInputValue = document.getElementById("tune-title");
 
 	addLink.addEventListener("click", function() {
     console.log("addForm.js:10");
@@ -16,5 +17,10 @@
     addView.classList.remove("hidden");
 
   });
+
+  var addButton = document.getElementById("add-button");
+  addButton.addEventListener("click", function() {
+    console.log("add button clicked, value = ", titleInputValue.value);
+  })
 
 })();
