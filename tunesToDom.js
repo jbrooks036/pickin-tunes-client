fefb9b7687a3bbcff179ed885420c5692bfc307d
Jo Ann Brooks <joannbrooks036@gmail.com>
2016-06-27 17:26:26 -0500

    WIP - creating and attaching click handler to Delete button for each tune in list

--- tunesToDom.js
+++ tunesToDom.js
@@ -19,17 +19,24 @@ var Pickin = (function (pickin) {
     // populate table
     for (var i = 0; i < numTunes; i++)
     {
-      // console.log("tunes[i] = ", i, tunes[i]);
-      var tableRow = "<tr><td>" + tunes[i].TuneId
+      var tuneId = tunes[i].TuneId;
+      // console.log("tuneId = ", tuneId);
+      var str = "id=" + tuneId;
+      console.log("str = ", str);
+      console.log("long phrase = ",  "<button type=&quot;button&quot; class=&quot;btn btn-default btn-xs &quot;;");
+      var tableRow = "<tr><td>" + tuneId
         // + "</td><td>"
         // + tunes[i].artist
         + "</td><td>"
         + tunes[i].TuneTitle
         + "</td><td>"
-        + "<button type=&quot;button&quot; class=&quot;btn btn-default btn-xs&quot;>Delete</button>" 
-        // + tunes[i].album
+//        + "<button type=&quot;button&quot; class=&quot;btn btn-default btn-xs&quot; id=tuneId;>Delete</button>" 
+        + "<button type=&quot;button&quot; class=&quot;btn btn-default btn-xs &quot; ;"
+        + str 
+        + ">Delete</button>" 
+          // + tunes[i].album
         + "</td></tr>";
-      console.log("tableRow[i] = ", i, tunes[i]);
+      console.log("tableRow[i] = ", i, tableRow);
       cumTable += tableRow;
 
     }
@@ -55,24 +62,34 @@ var Pickin = (function (pickin) {
       currentRow.onclick = createClickHandler(currentRow);
     }
 
-/*
-    // and then add "Delete" buttons
-    for (i = 0; i < rows.length; i++) {
-      var currentRow = tunesTable.rows[i];
-      var createClickHandler = 
-        function(row) {
-          console.log("inside createClickHandler");
+    // to setup Delete click handlers, first get all the Delete buttons
+    var tableDeleteButtons = tunesTable.getElementsByTagName("button");
+    console.log("tableDeleteButtons = ", tableDeleteButtons);
+
+    // then attach delete functionality to each Delete button
+    for (i = 0; i < numTunes; i++) {
+
+      // create the clickhandler
+      var createDeleteClickHandler = 
+        function(button) {
+          console.log("inside createDeleteClickHandler");
           return function() { 
-            console.log("clickHandler called");
-            var cell = row.getElementsByTagName("td")[0];
-            var id = cell.innerHTML;
-            alert("id:" + id);
+            console.log("returning function on button ", button);
+            var idStrings = button.getElementsByTagName("id");
+            var deleteId = parseInt(idStrings[0]);
+            alert("deleteId:" + deleteId);
           };
         };
-      currentRow.onclick = createClickHandler(currentRow);
-    }
-*/
 
+      // get the Delete button
+      var currentDeleteButton = tableDeleteButtons[i];
+      console.log("currentDeleteButton = ", currentDeleteButton);
+
+      // and attach the clickhandler
+      currentDeleteButton.onclick = createDeleteClickHandler(currentDeleteButton);
+      console.log("currentDeleteButton = ", currentDeleteButton);
+
+    }
 
   }
 
